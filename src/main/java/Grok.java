5eb81519f660f8c884fbdd0816ff5bbb60951764
Alec <mcflrrie@gmail.com>
2024-02-16 17:20:09 -0500

    Complete

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -4,10 +4,66 @@
  * a PowerPill to replenish their energy.  This makes them difficult
  * to kill.
  *
- * @author (You Again)
+ * @author (Alec Nieves)
  * @version (0.1)
  */
 public class Grok
 {
-    // implement this class per instructions in Required Activities.
+    private static final int DEFAULT_POWER_LEVEL = 50;
+    private int powerLevel;
+
+    /**
+     * \* Initializes a Grok object to the default power level of 50.
+     */
+
+    public Grok(){
+        this.powerLevel = DEFAULT_POWER_LEVEL;
+    }
+
+    /**
+     *
+     * Initializes a Grok object to power powerLevel
+     */
+    public Grok( int power) {
+        this.powerLevel = power;
+    }
+
+
+    /*
+     * Returns the power level of this Grok.
+     * @return returns the power level of this Grok
+     */
+
+    public int getPowerLevel() {
+        return powerLevel;
+    }/*
+     * Sets the power level of this Grok.
+     * @param powerLevel the power value to set for this Grok.
+     */
+
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+
+
+    /*
+     * Add the power level of this Grok to the power level of
+     * the pill.
+     * @param pill The PowerPill that the this Grok.  The power
+     * of the pill is added to the power level of this Grok.
+     */
+    public void takePowerPill(PowerPill pill) {
+        this.powerLevel += pill.getPower();
+    }
+
+    /*
+     * Invoked when this Grok takes a hit.  The power level of
+     * this Grok is reduced by 5.
+     */
+    public void tookHit() {
+        this.powerLevel -= 5;
+    }
 }
+
+
+
